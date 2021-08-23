0e8e5517e11bed344c659676d09a9fe161bd3340
Sato Ryoka <satoryoka0920@gmail.com>
2021-08-23 17:11:38 +0900

    tailwind css

--- App.tsx
+++ App.tsx
@@ -3,14 +3,16 @@ import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import { Provider } from "react-redux"
 import { store } from "./store"
+import HomeScreen from "./screens/HomeScreen"
 
 export default function App() {
   return (
     //@ts-ignore
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Lets build UBER!</Text>
-        <StatusBar style="auto" />
+        {/* <Text>Lets build UBER!</Text>
+        <StatusBar style="auto" /> */}
+        <HomeScreen />
       </View>
     </Provider>
   )
@@ -18,9 +20,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center"
   }
 })
